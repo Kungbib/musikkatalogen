d80c6091845c9070890912ce0a827d7df2253992
Peter Krantz <peter@peterkrantz.se>
2014-08-14 14:54:53 +0200

    help texts for admin

--- cards/models.py
+++ cards/models.py
@@ -9,6 +9,8 @@ class Catalog(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     is_alpha = models.BooleanField(default=False)
 
+    def __unicode__(self):
+        return u'%s' % self.name
 
     class Meta:
         verbose_name = u"Katalog"
@@ -21,6 +23,9 @@ class Signum(models.Model):
     description = models.TextField()
     slug = models.SlugField(max_length=50, unique=True)
 
+    def __unicode__(self):
+        return u'%s' % self.name
+
     class Meta:
         verbose_name = u"Signum"
         verbose_name_plural = u"Signum"
@@ -28,15 +33,18 @@ class Signum(models.Model):
 
 
 class Box(models.Model):
-    folder_name = models.CharField(max_length=255, unique=True)
+    folder_name = models.CharField(max_length=255, unique=True, verbose_name="Katalognamn", help_text="Filkatalog på disk där denna lådas filer ligger")
     sequence_number = models.IntegerField(db_index=True)
     slug = models.SlugField(max_length=50, unique=True)
     #from_text = models.CharField(max_length=255,db_index=True)
     #to_text = models.CharField(max_length=255,db_index=True)
-    label = models.CharField(max_length=255,db_index=True)
-    catalog = models.ForeignKey(Catalog, related_name="boxes")
+    label = models.CharField(max_length=255,db_index=True, verbose_name="Etikett")
+    catalog = models.ForeignKey(Catalog, related_name="boxes", verbose_name="Tillhör katalog")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __unicode__(self):
+        return u'%s %s' % (self.sequence_number, self.label)
 
     class Meta:
         verbose_name = u"Låda"
@@ -46,29 +54,33 @@ class Box(models.Model):
 
 
 class Card(models.Model):
-    name = models.CharField(max_length=255, db_index=True)
-    filename = models.CharField(max_length=255, db_index=True)
-    ocr_text = models.TextField(blank=True)
-    letter = models.CharField(max_length=1, null=True, blank=True, db_index=True)
-    sequence_number = models.IntegerField(db_index=True)
-    catalog_sequence_number = models.IntegerField(null=True, blank=True)
-    box = models.ForeignKey(Box, related_name="cards")
+    name = models.CharField(max_length=255, db_index=True, verbose_name="Kortnamn", help_text="Rubriken som visas överst på en kortsida")
+    filename = models.CharField(max_length=255, db_index=True, verbose_name="Filnamn", help_text="Filnamnet för bildfilen")
+    ocr_text = models.TextField(blank=True, help_text="Automatiskt OCR-tolkad text från kortet.")
+    letter = models.CharField(max_length=1, null=True, blank=True, db_index=True, verbose_name="Indexbokstav" , help_text="Anges för första kortet för att dela upp katalogen alfabetiskt.")
+    sequence_number = models.IntegerField(db_index=True, verbose_name="Sekvensnummer i låda")
+    catalog_sequence_number = models.IntegerField(null=True, blank=True, verbose_name="Kortnummer", help_text="Globalt katalognummer som anger kortets plats i katalogen. Används även som identifierare.")
+    box = models.ForeignKey(Box, related_name="cards", verbose_name="Låda")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    signum = models.ForeignKey(Signum, related_name="cards", null=True, blank=True)
+    signum = models.ForeignKey(Signum, related_name="cards", null=True, blank=True, verbose_name="Signum (ej i bruk än)")
+
 
     # Fields with manual transcription data
-    name_tr = models.CharField(max_length=255, db_index=True, blank=True, null=True)
-    arr_tr = models.CharField(max_length=255, db_index=True, blank=True, null=True)
-    pseudonym_tr = models.CharField(max_length=255, db_index=True, blank=True, null=True)
-    title_tr = models.CharField(max_length=255, db_index=True, blank=True, null=True)
-    comment = models.TextField(blank=True, null=True)
+    name_tr = models.CharField(max_length=255, db_index=True, blank=True, null=True, verbose_name="Namn (tranksriberat)", help_text="Transkriberat namn")
+    arr_tr = models.CharField(max_length=255, db_index=True, blank=True, null=True, verbose_name="Arrangemang av", help_text="Transkriberad information om arrangemangets upphovsmän")
+    pseudonym_tr = models.CharField(max_length=255, db_index=True, blank=True, null=True, verbose_name="Pseudonym", help_text="Transkriberad pseudonym")
+    title_tr = models.CharField(max_length=255, db_index=True, blank=True, null=True, verbose_name="Verkets namn", help_text="Transkriberad version av verkets namn")
+    comment = models.TextField(blank=True, null=True, verbose_name="Intern kommentar", help_text="Visas ej för besökare.")
 
     # readonly field to show preview pic in django admin interface
     def image_tag(self):
-        return u'<img alt="Kort %s" src="/static/alfa/%s" />' % (self.name, self.box.folder_name + "/" + self.filename.replace(".jpg", "_view500.jpg"))
+        return u'<img alt="Kort %s" src="/static/alfa/%s" />' % (self.catalog_sequence_number, self.box.folder_name + "/" + self.filename.replace(".jpg", "_view500.jpg"))
     image_tag.short_description = 'Bild'
     image_tag.allow_tags = True
+
+    def __unicode__(self):
+        return u'%s %s' % (self.catalog_sequence_number, self.name)
 
     class Meta:
         verbose_name = u"Kort"
